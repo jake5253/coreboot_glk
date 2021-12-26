9ab8aeadf28600e44d87c702ee4565381e6f9adf
jake5253 <jake.admin@gmail.com>
2021-12-26 01:27:48 +0000

    stupid

---
+++ startup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "Installing Dependencies"
+sudo apt update -qqq
+sudo apt install --yes -qqq \
+    git \
+    build-essential \
+    gnat \
+    flex \
+    bison \
+    libncurses5-dev \
+    wget \
+    zlib1g-dev \
+    sharutils \
+    e2fsprogs \
+    parted \
+    curl \
+    unzip \
+    ca-certificates
+
+echo "Obtaining Coreboot Source"
+git clone --quiet https://review.coreboot.org/coreboot
+cd /workspace/coreboot_glk/coreboot
+git submodule --quiet update --init --checkout
+
+echo "Building Coreboot crosstools"
+cd /workspace/coreboot_glk/coreboot
+make crosstools-x86 CPUS=$(nproc) > /dev/null
+
+echo "Installing Helper Tools"
+cd /workspace/coreboot_glk/coreboot/util/cbfstool
+make > /dev/null
+sudo make install > /dev/null 
+cd /workspace/coreboot_glk/coreboot/util/ifdtool
+make > /dev/null
+sudo make install > /dev/null
+
+echo "Preparing Firmware Blobs"
+cd /workspace/coreboot_glk
+/workspace/coreboot_glk/crosfirmware.sh octopus glk
+
+echo "Finished"
+#run builder 
