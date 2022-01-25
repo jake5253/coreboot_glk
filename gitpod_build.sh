112587a12a37cbe0bc7188f7b05e69a6225ec4c5
jake5253 <jake.admin@gmail.com>
2022-01-25 16:54:24 -0500

    add buildlog

--- gitpod_build.sh
+++ gitpod_build.sh
@@ -2,7 +2,7 @@
 
 # Main dependencies
 echo "Installing Dependencies"
-sudo apt update
+sudo apt update --qq 2>&1 > /tmp/buildlog
 sudo apt install --yes \
     git \
     build-essential \
@@ -17,32 +17,36 @@ sudo apt install --yes \
     parted \
     curl \
     unzip \
-    ca-certificates
+    ca-certificates \
+    --qq 2>&1 > /tmp/buildlog
+    
+echo "At any time, if you wish to view the builder log, you can open your browser to:"
+gp url 8123
 
 # coreboot source
 echo "Obtaining Coreboot Source"
-git clone --quiet https://review.coreboot.org/coreboot
+git clone https://review.coreboot.org/coreboot 2>&1 > /tmp/buildlog
 
 # coreboot submodules
 echo "Getting Coreboot submodules" 
 cd coreboot 
-git submodule --quiet update --init --checkout
+git submodule update --init --checkout 2>&1 > /tmp/buildlog
 
 # crosstools
 echo -e "Building Coreboot crosstools.\n This may take a few minutes"
-make crossgcc-i386 CPUS=$(nproc) > /dev/null
+make crossgcc-i386 CPUS=$(nproc) 2>&1 > /tmp/buildlog
 
 # coreboot tools
 echo "Installing Helper Tools" 
 # CBFSTool
 cd /util/cbfstool 
-make > /dev/null 
-sudo make install > /dev/null
+make 2>&1 > /tmp/buildlog
+sudo make install 2>&1 > /tmp/buildlog
 
 # IFDTool
 cd ../ifdtool 
-make > /dev/null 
-sudo make install > /dev/null
+make 2>&1 > /tmp/buildlog 
+sudo make install 2>&1 > /tmp/buildlog
 
 # Process blobs
 cd /workspace/coreboot_glk 
