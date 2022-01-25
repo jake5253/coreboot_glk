487af49b4c3d02293e060bb10c3ffd3aaa1ff63e
jake5253 <jake.admin@gmail.com>
2022-01-25 18:28:02 -0500

    Update gitpod_build.sh

--- gitpod_build.sh
+++ gitpod_build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+[[ -f logger/build.log ]] && rm logger/build.log || touch logger/build.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>logger/build.log 2>&1
 
 # Main dependencies
-echo "Installing Dependencies"
-sudo apt update 2>&1 > /tmp/buildlog
+echo "Installing Dependencies" >&3
+sudo apt update
 sudo apt install --yes \
     git \
     build-essential \
@@ -18,42 +22,47 @@ sudo apt install --yes \
     curl \
     unzip \
     ca-certificates \
-    2>&1 > /tmp/buildlog
+    python3-pip
     
-echo "At any time, if you wish to view the builder log, you can open your browser to:"
-gp url 8123
+# Set up a python server to display "tail -f" in a browser window
+python3 -m pip install websockets
+nohup python3 -u logger/tail.py webserver ./build.log > logger/pythonwebserver.log &
+nohup python3 -u logger/tail.py tailserver ./build.log > logger/pythontailserver.log &
+
+echo "At any time, if you wish to view the builder log, you can open your browser to:" >&3
+gp url 8123 >&3
 
 # coreboot source
-echo "Obtaining Coreboot Source"
-git clone https://review.coreboot.org/coreboot 2>&1 > /tmp/buildlog
+echo "Obtaining Coreboot Source" >&3
+git clone https://review.coreboot.org/coreboot
 
 # coreboot submodules
-echo "Getting Coreboot submodules" 
+echo "Getting Coreboot submodules" >&3
 cd coreboot 
-git submodule update --init --checkout 2>&1 > /tmp/buildlog
+git submodule update --init --checkout
 
 # crosstools
-echo -e "Building Coreboot crosstools.\n This may take a few minutes"
-make crossgcc-i386 CPUS=$(nproc) 2>&1 > /tmp/buildlog
+echo -e "Building Coreboot crosstools.\nThis may take a few minutes" >&3
+make crossgcc-i386 CPUS=$(nproc)
 
 # coreboot tools
-echo "Installing Helper Tools" 
+echo "Installing Helper Tools" >&3
 # CBFSTool
 cd /util/cbfstool 
-make 2>&1 > /tmp/buildlog
-sudo make install 2>&1 > /tmp/buildlog
+make
+sudo make install
 
 # IFDTool
 cd ../ifdtool 
-make 2>&1 > /tmp/buildlog 
-sudo make install 2>&1 > /tmp/buildlog
+make 
+sudo make install
 
 # Process blobs
 cd /workspace/coreboot_glk 
-echo "Preparing Firmware Blobs" 
+echo "Preparing Firmware Blobs" >&3
 bash ./crosfirmware.sh octopus glk
 
-echo "Setup has completed"
+echo "Setup has completed" >&3
 
 # Launch build script
 bash ./build.sh
