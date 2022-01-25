63639d6d58fe1bf7f0a5e1b48b70a7cf05fd6265
jake5253 <jake.admin@gmail.com>
2022-01-25 02:45:38 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -22,19 +22,19 @@ sudo apt install --yes \
     ca-certificates
 
 echo "Obtaining Coreboot Source"
-git clone --quiet https://review.coreboot.org/coreboot "${SCRIPT_DIR}/coreboot_glk"
-cd "${SCRIPT_DIR}/coreboot_glk"
+git clone --quiet https://review.coreboot.org/coreboot "${SCRIPT_DIR}/coreboot"
+cd "${SCRIPT_DIR}/coreboot"
 git submodule --quiet update --init --checkout
 
 echo "Building Coreboot crosstools"
-cd "${SCRIPT_DIR}/coreboot_glk"
+cd "${SCRIPT_DIR}/coreboot"
 make crossgcc-i386 CPUS=$(nproc) > /dev/null
 
 echo "Installing Helper Tools"
-cd "${SCRIPT_DIR}/coreboot_glk/util/cbfstool"
+cd "${SCRIPT_DIR}/coreboot/util/cbfstool"
 make > /dev/null
 sudo make install > /dev/null 
-cd "${SCRIPT_DIR}/coreboot_glk/util/ifdtool"
+cd "${SCRIPT_DIR}/coreboot/util/ifdtool"
 make > /dev/null
 sudo make install > /dev/null
 
