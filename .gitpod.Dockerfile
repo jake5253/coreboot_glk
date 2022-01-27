7a81431e400a9148ece30f1208140d6f3269c36d
jake5253 <jake.admin@gmail.com>
2022-01-27 21:56:41 +0000

    change to Dockerfile setup

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,47 @@
+FROM gitpod/workspace-full
+
+ARG WORKDIRBASE
+ENV WORKDIRBASE=${WORKDIRBASE:-/workspace/coreboot_glk}
+
+# Install custom tools, runtime, etc.
+RUN echo "Installing dependencies" \
+    && sudo apt-get update \
+    && sudo apt-get install -y \
+        git \
+        build-essential \
+        gnat-10 \
+        flex \
+        bison \
+        libncurses5-dev \
+        wget \
+        zlib1g-dev \
+        sharutils \
+        e2fsprogs \
+        parted \
+        curl \
+        unzip \
+        ca-certificates \
+    && sudo rm -rf /var/lib/apt/lists/*
+
+WORKDIR $WORKDIRBASE
+RUN echo "Obtaining Coreboot source and submodules" \
+    && git clone https://review.coreboot.org/coreboot \
+    && cd coreboot \
+    && git submodule update --init --checkout
+
+WORKDIR coreboot
+RUN echo -e "Building Coreboot crossgcc.\nThis could take a while (10-15 minutes)" \
+    && make crossgcc-i386 CPUS=$(nproc)
+
+RUN echo "Building and installing helper tools"
+
+WORKDIR $WORKDIRBASE/coreboot/util/cbfstool
+RUN make \
+    && sudo make install
+
+WORKDIR $WORKDIRBASE/coreboot/util/ifdtool
+RUN make \
+    && sudo make install
+
+WORKDIR $WORKDIRBASE
+ENTRYPOINT exec bash build.sh
--- build.sh
+++ build.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
 # The following line determines if the script was run directly or sourced by gitpod_build.sh
-(return 0 2>/dev/null) && sourced=1 || sourced=0
+#(return 0 2>/dev/null) && sourced=1 || sourced=0
 
-[[ $sourced == 0 ]] && {
-exec 3>&1 4>&2;
-trap 'exec 2>&4 1>&3' 0 1 2 3;
-exec 1>>logger/build.log 2>&1;
-}
+#[[ $sourced == 0 ]] && {
+#exec 3>&1 4>&2;
+#trap 'exec 2>&4 1>&3' 0 1 2 3;
+#exec 1>>logger/build.log 2>&1;
+#}
 
 # This file assumes it is located in a directory parallel to coreboot_glk
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+do_crosfirmware()
+{
+    bash ${SCRIPT_DIR}/crosfirmware.sh octopus glk
+}
 
 do_build()
 {
     _dev=$1
     cd "${SCRIPT_DIR}/coreboot"
-    [[ $sourced == 0 ]] && make distclean
+    make distclean
     cp configs/config.$_dev .config
     make olddefconfig
     make CPUS=$(nproc)
 }
 
 
-devices=($(cat devices))
+devices=($(cat "${SCRIPT_DIR}/devices"))
+while [ -z $devices ]; do
+    do_crosfirmware
+    devices=($(cat "${SCRIPT_DIR}/devices"))
+done
 while [ -z $dev ]; do
     echo "Choose one of the following devices for the build:"
-    echo "Choose one of the following devices for the build:" >&3
     for ((i = 0; i < ${#devices}; i++)); do
         echo "${devices[$i]}";
-        echo "${devices[$i]}" >&3
     done
-    read -r -p "Selection : " dev >&3
+    read -r -p "Selection : " dev
     for ((i = 0; i < ${#devices}; i++)); do
         if [[ ${devices[$i]} = "${dev,,}" ]]; then
             do_build $dev
             break
+        elif [[ ${dev,,} == ^(x|exit)$ ]]; then
+            exit
         fi
     done
 
@@ -45,8 +54,9 @@ while [ -z $dev ]; do
     fi
 done
 
-echo "Build done"
-echo "Build process completed. A window will pop open containing your compiled firmware so you can download and store the file locally" >&3
-echo >&3
-echo "To build for another device, run build.sh again." >&3
+echo -e "Build process completed. \
+    \nA window will pop open containing your compiled \
+    \nfirmware so you can download and store the file locally"
+echo 
+echo "To build for another device, run build.sh again."
 python3 -m http.server --directory coreboot 3000
