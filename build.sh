89137776cf64dc6202b1ced3d918045ed757f7b0
jake5253 <jake.admin@gmail.com>
2022-06-12 19:24:00 +0000

    more changes

--- build.sh
+++ build.sh
@@ -2,46 +2,41 @@
 
 # This file assumes it is located in a directory parallel to coreboot_glk
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+GITPOD_REPO_ROOT=${GITPOD_REPO_ROOT:-.}
 LOG_DIR=${GITPOD_REPO_ROOT}/log
 
-echo ${SCRIPT_DIR}
+echo "Logs will be stored in: ${LOG_DIR}"
 
 do_crosfirmware()
 {
-    bash ${SCRIPT_DIR}/crosfirmware.sh
-}
-
-build_crossgcc()
-{
-    cd "${GITPOD_REPO_ROOT}/coreboot"
-    echo "Building the coreboot toolchain"
-    echo "This may take a while"
-    touch $LOG_DIR/crossgcc.log
-    LOGPID=$( (nohup python $LOG_DIR/log_stream.py $LOG_DIR/crossgcc.log >/dev/null 2>&1 & echo $!) )
-    echo "LOGPID = ${LOGPID}"
-    (nohup make crossgcc-i386 CPUS=$(nproc) >$LOG_DIR/crossgcc.log 2>&1 & ${GITPOD_REPO_ROOT}/spinner.sh $! 2>/dev/null)
-    kill -9 $LOGPID
+    [[ -f crosfirmware.sh ]] && bash crosfirmware.sh || { echo "ERROR! crosfirmware.sh file not found!"; exit 1; }
 }
 
 build_coreboot()
 {
-    _dev=$1
+    _dev=${1:? "device not specified!"}
     cd "${GITPOD_REPO_ROOT}/coreboot"
-    cp configs/config.$_dev .config
+    cp configs/config.$_dev .config || { echo "File not found: $PWD/configs/config.$_dev"; exit 1; }
     make olddefconfig
     touch $LOG_DIR/coreboot.log
-    LOGPID=$( (nohup python $LOG_DIR/log_stream.py $LOG_DIR/coreboot.log >/dev/null 2>&1 & echo $!) )
-    echo "LOGPID = ${LOGPID}"
-    (nohup make CPUS=$(nproc) 2>&1 >$LOG_DIR/coreboot.log &)
-        ${GITPOD_REPO_ROOT}/spinner.sh $! 2>/dev/null
+    LOGPID=$( ( nohup python $LOG_DIR/log_stream.py $LOG_DIR/coreboot.log >/dev/null 2>&1 & echo $! ) )
+    ( nohup make CPUS=$(nproc) 2>&1 >$LOG_DIR/coreboot.log & ${GITPOD_REPO_ROOT}/spinner.sh $! 2>/dev/null )
     kill -9 $LOGPID
 }
 
-build_crossgcc || { echo "ERROR! crossgcc toolchain build failed!"; exit 1; }
+showfiles()
+{
+    echo -e "Build process completed. \
+    \nA window will pop open containing your compiled \
+    \nfirmware so you can download and store the file locally"
+    echo 
+    echo "To build for another device, run build.sh again."
+    python3 -m http.server --directory ./ 3000
+}
 
-[[ ! -f devices ]] && { do_crosfirmware; sync; }
+[[ ! -f ${GITPOD_REPO_ROOT}/devices ]] && { do_crosfirmware; sync; }
 
-devices=($(cat devices | sort -u))
+devices=( $(cat ${GITPOD_REPO_ROOT}/devices | sort -u) )
 
 while [ -z $dev ]; do
     clear
@@ -54,7 +49,7 @@ while [ -z $dev ]; do
     read -r -p "Selection: " dev
     for ((i = 0; i < ${#devices[*]}; i++)); do
         if [[ ${devices[$i]} = "${dev,,}" ]]; then
-            build_coreboot $dev || { echo "ERROR!! Coreboot build failed!"; exit 1; }
+            build_coreboot $dev && showfiles || { echo "ERROR!! Coreboot build failed!"; exit 1; }
             break
         elif [[ ${dev,,} =~ ^(x|exit)$ ]]; then
             exit
@@ -64,16 +59,5 @@ while [ -z $dev ]; do
             break
         fi
     done
-
-    if ((i == ${#devices[*]})); then
-        unset dev
-        continue
-    fi
+    unset dev
 done
-
-echo -e "Build process completed. \
-    \nA window will pop open containing your compiled \
-    \nfirmware so you can download and store the file locally"
-echo 
-echo "To build for another device, run build.sh again."
-python3 -m http.server --directory ./ 3000
